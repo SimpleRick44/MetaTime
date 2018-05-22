cedbc8e2cf074946bc428f4c3c401a9d5e1c3810
blcklght <luca.waldems@gmail.com>
2018-05-22 16:38:01 +0200

    Border Radius

--- lib/pages/zeitmanu_page.dart
+++ lib/pages/zeitmanu_page.dart
@@ -6,15 +6,19 @@ class Manu extends StatelessWidget {
     return new Container(
       padding: const EdgeInsets.all(80.0),
       margin: const EdgeInsets.only(right: 100.0),
-        child: new TextField(
-          style: const TextStyle(),
-          decoration: new InputDecoration(
-            border:const UnderlineInputBorder(),
+      child: new TextField(
+        style: const TextStyle(),
+        decoration: new InputDecoration(
+            border: const UnderlineInputBorder(
+                borderRadius: const BorderRadius.only(
+                    bottomRight: const Radius.circular(15.0),
+                    bottomLeft: const Radius.circular(15.0),
+                    topLeft: const Radius.circular(15.0),
+                    topRight: const Radius.circular(15.0))),
             filled: true,
-            labelText: 'Stunden'
-          ),
-          keyboardType: TextInputType.number,
-        ),
-      );
+            labelText: 'Stunden:'),
+        keyboardType: TextInputType.number,
+      ),
+    );
   }
 }
