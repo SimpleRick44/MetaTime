c60c45e9accbaaaf38d86302164eea9cb9adeaf9
blcklght <luca.waldems@gmail.com>
2018-05-29 11:55:18 +0200

    Google Api-Key

--- lib/pages/zeitauto_page.dart
+++ lib/pages/zeitauto_page.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class Auto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+     _getTimeApi() async {
+      var httpClient = new HttpClient();
+      var uri = new Uri.https('https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=AIzaSyDdSna-wkK3zLS7QrjwSap2-voSZQOtSSY', unencodedPath)
+    } 
     return new Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         new RaisedButton(
-          padding: const EdgeInsets.only(
-              top: 30.0, bottom: 30.0, left: 60.0, right: 60.0),
-          child: const Icon(Icons.timer),
-          color: Colors.green,
-          elevation: 4.0,
-          splashColor: Colors.greenAccent,
-          onPressed: () {
-            print("Start");
-          },
-        ),
+            padding: const EdgeInsets.only(
+                top: 30.0, bottom: 30.0, left: 60.0, right: 60.0),
+            child: const Icon(Icons.timer),
+            color: Colors.green,
+            elevation: 4.0,
+            splashColor: Colors.greenAccent,
+            onPressed: () {
+              print("Start");
+            }),
         new Column(
           children: <Widget>[
             new Container(
