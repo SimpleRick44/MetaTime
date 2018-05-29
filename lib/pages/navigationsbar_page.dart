4a26c01a9fc5d72a86de25d795ca404d3dc4a9aa
blcklght <luca.waldems@gmail.com>
2018-05-29 10:22:30 +0200

    nav

--- lib/pages/navigationsbar_page.dart
+++ lib/pages/navigationsbar_page.dart
@@ -2,17 +2,12 @@ import 'package:flutter/material.dart';
 import './zeitauto_page.dart' as auto;
 import './zeitmanu_page.dart' as manu;
 
-
-
-
 class MyTabs extends StatefulWidget {
   @override
   MyTabsState createState() => new MyTabsState();
 }
 
 class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
-
-
   TabController controller;
 
   @override
@@ -26,43 +21,46 @@ class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin {
     controller.dispose();
     super.dispose();
   }
- @override
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Zeit erfassen"),backgroundColor: Colors.grey,
-        actions: <Widget>[
-          new PopupMenuButton(
-            icon: new Icon(Icons.arrow_drop_down),
-            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-              const PopupMenuItem(
-                child: const Text('Test')
-              ),
-              const PopupMenuItem(
-                child: Text('Test2'),
+        appBar: new AppBar(
+            title: new Text("Zeit erfassen"),
+            backgroundColor: Colors.grey,
+            actions: <Widget>[
+              new DropdownButton(
+                items: <String>['Test', 'Test2'].map((String value) {
+                  return new DropdownMenuItem<String>(
+                    value: value,
+                    child: new Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
               )
-            ]
-          )
-        ],
-      ),
-      bottomNavigationBar: new Material(
-        color: Colors.white,
-        child: new TabBar(
-          controller: controller,
-          labelColor: Colors.grey,
-          tabs: <Tab>[
-            new Tab(text: "Automatisch", icon: new Icon(Icons.timer, color: Colors.grey,)),
-            new Tab(text: "Manuell", icon: new Icon(Icons.edit, color: Colors.grey,)),
-          ]
-        )
-      ),
-      body: new TabBarView(
-        controller: controller,
-        children: <Widget>[
-           new auto.Auto(),
-           new manu.Manu(),
-        ]
-      )
-    );
+            ]),
+        bottomNavigationBar: new Material(
+            color: Colors.white,
+            child: new TabBar(
+                controller: controller,
+                labelColor: Colors.grey,
+                tabs: <Tab>[
+                  new Tab(
+                      text: "Automatisch",
+                      icon: new Icon(
+                        Icons.timer,
+                        color: Colors.grey,
+                      )),
+                  new Tab(
+                      text: "Manuell",
+                      icon: new Icon(
+                        Icons.edit,
+                        color: Colors.grey,
+                      )),
+                ])),
+        body: new TabBarView(controller: controller, children: <Widget>[
+          new auto.Auto(),
+          new manu.Manu(),
+        ]));
   }
 }
