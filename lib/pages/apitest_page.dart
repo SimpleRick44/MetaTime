ffb7e510174e06621350d2f1b3dc116bcf4a935c
blcklght <luca.waldems@gmail.com>
2018-06-04 12:18:15 +0200

    DateTime

--- lib/pages/apitest_page.dart
+++ lib/pages/apitest_page.dart
@@ -10,45 +10,40 @@ class Kunden extends StatefulWidget {
 }
 
 class KundenPageState extends State<Kunden> {
-
   String displayedsring = "";
 
   var data;
 
   Future<String> getData() async {
     var response = await http.get(
-      Uri.encodeFull("http://api.timezonedb.com/v2/get-time-zone?key=3NEWA7XBCFKW&format=json&by=zone&zone=Europe/Berlin"),
-      headers: {
-        "Accept": "application/json"
-      }
-    );
+        Uri.encodeFull(
+            "http://api.timezonedb.com/v2/get-time-zone?key=3NEWA7XBCFKW&format=json&by=zone&zone=Europe/Berlin"),
+        headers: {"Accept": "application/json"});
 
-    
-      data = JSON.decode(response.body);
-         setState(() {
-        displayedsring = data["formatted"];
-      });
+    data = JSON.decode(response.body);
+    setState(() {
+      displayedsring = data["formatted"];
+    });
     return "Success!";
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Kunden"),
-      ),
-      body: new Center(
-     child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Text(displayedsring),
-          new RaisedButton(
-            child: new Text('GetData'),
-            onPressed: getData,
-          )
-        ],
-      )
-      )
-    );
+        appBar: new AppBar(
+          title: new Text("Kunden"),
+        ),
+        body: new Center(
+            child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(displayedsring),
+            new RaisedButton(
+                child: new Text('GetData'),
+                onPressed: () {
+                  print(new DateTime.now());
+                })
+          ],
+        )));
   }
 }
