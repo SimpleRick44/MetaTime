729047faabbc8817557e90ea67237cd216d6e86f
RiccardoMetaTime <riccardo77@t-online.de>
2018-06-19 11:14:06 +0200

    NEW

--- lib/pages/zeitmanu_page.dart
+++ lib/pages/zeitmanu_page.dart
@@ -72,55 +72,31 @@ class Manu extends StatelessWidget {
               )
             ],
           ),
-          new TextField(
-            decoration: InputDecoration(
-                contentPadding: const EdgeInsets.only(
-                    right: 10.0, left: 10.0, top: 10.0, bottom: 30.0),
-                border: const UnderlineInputBorder(
-                    borderRadius: const BorderRadius.only(
-                        bottomRight: const Radius.circular(15.0),
-                        bottomLeft: const Radius.circular(15.0),
-                        topLeft: const Radius.circular(15.0),
-                        topRight: const Radius.circular(15.0))),
-                filled: true,
-                labelText: 'Tätigkeiten'),
-          ),
-          new Column(
-            children: <Widget>[
-              new Container(
-                padding: const EdgeInsets.only(top: 20.0),
-              )
-            ],
-          ),
-          new DropdownButton(
-            
-            hint: new Text("Kunde"),
-            items: <String>['TestFirma', 'TestFirma2'].map((String value) {
-              return new DropdownMenuItem<String>(
-                value: value,
-                child: new Text(value),
-              );
-            }).toList(),
-            onChanged: (_) {},
-          ),
-          new Column(
-            children: <Widget>[
-              new Container(
-                padding: const EdgeInsets.only(top: 90.0),
-              )
-            ],
-          ),
-          new RaisedButton(
-            padding: const EdgeInsets.only(
-                top: 15.0, bottom: 15.0, left: 40.0, right: 40.0),
-            child: const Text('Speichern'),
-            color: Theme.of(context).accentColor,
-            elevation: 4.0,
-            splashColor: Colors.blue,
-            onPressed: () {
-              print("test");
-            },
-          )
+           new Text(
+        "Tätigkeit",
+        style: new TextStyle(
+          fontSize: 50.0,
+        ),
+        textAlign: TextAlign.left,
+      ),
+      new TextFormField(
+          keyboardType: TextInputType.multiline,
+          maxLines: 15,
+          maxLength: 1150,
+          maxLengthEnforced: true,
+          autocorrect: true,
+          textAlign: TextAlign.left,
+          style: new TextStyle(fontSize: 30.0, color: Colors.black),
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.only(bottom: 00.01),
+            border: const UnderlineInputBorder(
+                borderRadius: const BorderRadius.only(
+                    bottomRight: const Radius.circular(15.0),
+                    bottomLeft: const Radius.circular(15.0),
+                    topLeft: const Radius.circular(15.0),
+                    topRight: const Radius.circular(15.0))),
+            filled: true,
+          )),
         ],
       )
     ]);
